2063f512102a2b49f052b202b6ae3eef17626d41
gutina-zhang <qzha6629@uni.sydney.edu.au>
2025-06-12 05:57:06 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 // Yellow Rectangle
 let yellowRects = [];
+//* An array that stores all your yellow background lines (rectangles).
 let rectWidth = 20;
+//* The thickness of lines
 
 // Color
 let colors = [];
@@ -23,6 +25,7 @@ let lerpColors = [];
 let isStart = false;
 
 // Preload
+//* Preventation of of missing music when play the animation
 function preload() {
   reboundSound = loadSound("assets/rebound.m4a");
   bgmSound = loadSound("assets/bgm.mp3");
@@ -55,7 +58,7 @@ function setup() {
     { x: 520, y: 0, w: rectWidth, h: 320 },
     { x: 520, y: 380, w: rectWidth, h: 620 },
     { x: 600, y: 380, w: rectWidth, h: 180 },
-  ];
+  ]; //*The position and the size of the rectangles
 
   // Gradient color array
   lerpColorsPair = [
@@ -77,30 +80,58 @@ function setup() {
     [color(255, 192, 203), color(250,201, 1)],
     [color(255, 192, 203), color(250,201, 1)],
     [color(255, 192, 203), color(250,201, 1)],
-  ];
+  ]; //* To create a smooth blend between these two colors. These pairs correspond to background gradients.
 
   // Rebound big rectangle position, size, color, motor movement and rebound boarder
+  //* These lines below are large, animated rectangles.
   bigRects = [
     {
       x: 320, y: 190, 
       w: 100, h: 80,
-      c: colors[1],
+      c: colors[1], //Red
       dx: 2, dy: 0,
-      boundsStart: 540,
-      boundsEnd: width, 
+      boundsStart: 540, boundsEnd: width, //* The range where it can move — it will bounce back at the edges.
     },
     {x: 620, y: 190,
       w: 100, h: 80,
-      c: colors[1],
-      dx: 2, dy: 0,
-      boundsStart: 540,
-      boundsEnd: width, 
-    },
+      c: colors[1],  // color
+      dx: 2, dy: 0, // Moves horizontally
+      boundsStart: 540, boundsEnd: width, 
+    }, 
+
     {
-      
+      x: 620, y: 190,
+      w: 100, h: 80,
+      c: colors[2],
+      dx: 2, dy: 0,
+      boundsStart: 580, boundsEnd: height - 40, 
+    },
+  ];
+
+  // Randomly generate small moving rectangles in a yellow background
+  for (let i = 0; i < yellowRects.length; i++) {
+    let yellow = yellowRects[i];
+    // Only generate on rectangles that fill the screen horizontally or vertically
+    if (yellow.w == width) {
+      generateRandomRectangles(-1, yellow.y, "x");
+    } else if (yellow.h == height) {
+      generateRandomRectangles(yellow.x, -1, "y");
     }
-  ]
-}
+  }
+     //* Ensuring that small rectangles only move along full-length yellow bars, keeping the animation clean and organized.
+    
+  // Initialize the gradient color
+  for (let i = 0; i < lerpColorsPair.length; i++) {
+    let cList = lerpColorsPair[i];
+    lerpColors[i] = [];
+    for (let j = 0; j < 800; j++) {
+      let c = lerpColor(cList[0], cList[1], j / 800);
+      lerpColors[i].push(c);
+    }
+    }
+  }
+  
+  
 
 function draw() {
   background(220);
